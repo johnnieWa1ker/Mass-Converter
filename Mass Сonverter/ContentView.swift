9a19eceb61891cc786ece0f2e11f5deb26765b06
Johnnie Walker <rockwave.fm@gmail.com>
2020-10-07 09:33:22 +0300

    Add base feature

--- Mass Сonverter/ContentView.swift
+++ Mass Сonverter/ContentView.swift
@@ -8,9 +8,78 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var origin: String = ""
+    @State var selectedUnit: Int = 0
+    
+    var units = ["gram", "kilogram", "centner", "ton"]
+    
+    var doubleOrigin: Double {
+        return Double(origin) ?? 0
+    }
+    
+    var baseUnit: Double {
+        return doubleOrigin * 1000
+    }
+    
+    var converted: Double {
+        let multiplicator: Double
+        
+        switch self.selectedUnit {
+        case 0:
+            multiplicator = 1
+        case 1:
+            multiplicator = 0.001
+        case 2:
+            multiplicator = 0.00001
+        case 3:
+            multiplicator = 0.000001
+        default:
+            return 0
+        }
+        
+        return baseUnit * multiplicator
+    }
+    
+    var specifier: String {
+        switch self.selectedUnit {
+        case 0:
+            return "%.0f"
+        case 1:
+            return "%.2f"
+        case 2:
+            return "%.3f"
+        case 3:
+            return "%.4f"
+        default:
+            return "%.2f"
+        }
+    }
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            Form {
+                Section(header: Text("From kg")) {
+                    TextField("Enter value", text: $origin)
+                        .keyboardType(.decimalPad)
+                }
+                
+                Section(header: Text("What value should you convert to?")) {
+                    Picker("Select Unit", selection: $selectedUnit) {
+                        ForEach(0 ..< units.count) { unit in
+                            Text("\(self.units[unit])")
+                        }
+                    }
+                    .pickerStyle(SegmentedPickerStyle())
+                }
+                
+                Section(footer: Text("in \(doubleOrigin, specifier: "%.2f") kg.")) {
+                    Text("\(converted, specifier: self.specifier) \(units[selectedUnit])")
+                }
+                
+            }
+            .navigationTitle("Mass Converter")
+        }
     }
 }
 
---
+++ Mass Сonverter/README.md
@@ -0,0 +1,23 @@
+#  🔄 Mass Converter
+[![Build Status](https://img.shields.io/badge/Swift-5.3-orange.svg)](https://swift.org)
+
+A test project in which I am learning the SwiftUI framework.
+The project is part of Paul Hudson's training cycle - 100 Days of SwiftUI
+
+  <p float="right">
+  <img src="Mass-Converter/Assets.xcassets/demo-1.imageset/demo-1.png" width="32%"/>
+  </p>
+  
+
+## 🏃‍♂️ Getting Started
+
+``` bash
+git clone https://github.com/johnnieWa1ker/Mass-Converter.git
+cd Mass-Converter
+pod install
+open Mass-Converter.xcodeproj.
+```
+
+## 🎉 Why am I building this?
+1. I am very interested in the SwiftUI framework from Apple, I believe that after a while it will become quite powerful so that you can quickly make cool projects with it
+1. I want to master the 100 Days SwiftUI marathon 🏃‍♂️
