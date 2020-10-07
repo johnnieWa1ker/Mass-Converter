dfe05f14d4b5d93fa0e40c0a5af91e89ae953311
Johnnie Walker <rockwave.fm@gmail.com>
2020-10-07 08:13:48 +0300

    Initial Commit

---
+++ Mass Сonverter/Mass__onverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mass__onverterApp.swift
+//  Mass Сonverter
+//
+//  Created by Johnnie Walker on 10/7/20.
+//
+
+import SwiftUI
+
+@main
+struct Mass__onverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
